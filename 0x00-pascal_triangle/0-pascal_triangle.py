cdc8d033aacbe32a13a41f894ab784c910b37675
Omewah <joelomewah@ymail.com>
2024-01-09 12:47:25 +0100

    lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""returns a lists of integers representing the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    p_angle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+
+        for j in range(1, i):
+            row.append(p_angle[i - 1][j - 1] + p_angle[i - 1][j])
+
+        row.append(1)
+
+        p_angle.append(row)
+
+    return p_angle
