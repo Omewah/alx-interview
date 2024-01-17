e00160b16179a093a8927f8f1dbe9b058c1c7a1a
Omewah <joelomewah@ymail.com>
2024-01-17 18:16:23 +0100

    calculates the fewest number which represents the minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,15 +4,23 @@
 
 def minOperations(n):
     """Analyze the fewest number of ops to result in n H characters"""
-    if n <= 1:
+    if not isinstance(n, int):
         return 0
 
-    ops_num = [float('inf')] * (n + 1)
-    ops_num[1] = 0
+    ops_num = 0
+    dbase = 0
+    currH = 1
 
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                ops_num[i] = min(ops_num[i], ops_num[j] + 1 // j)
-
-    return ops_num[n] if ops_num[n] != float('inf') else 0
+    while currH < n:
+        if dbase == 0:
+            dbase = currH
+            currH += dbase
+            ops_num += 2
+        elif n - currH > 0 and (n - currH) % currH == 0:
+            dbase = currH
+            currH += dbase
+            ops_num += 2
+        elif dbase > 0:
+            currH += dbase
+            ops_num += 1
+    return ops_num
